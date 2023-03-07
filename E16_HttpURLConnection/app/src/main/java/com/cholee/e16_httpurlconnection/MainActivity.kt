7b60ceb8acb5bc7bf8adf9c5994a41e01ea68c8e
seungcheol <leesc5724@naver.com>
2023-03-07 17:13:21 +0900

    ➕[add]: HttpURLConnection Exam, Markdown

---
+++ E16_HttpURLConnection/app/src/main/java/com/cholee/e16_httpurlconnection/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cholee.e16_httpurlconnection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
